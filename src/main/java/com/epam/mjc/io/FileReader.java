09d8f407768e0b6633a2a0890a47b2cd71a1b8ee
islam <isejtniyazov@mail.ru>
2026-05-21 19:08:16 +0500

    fix

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,5 +1,6 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -8,19 +9,40 @@ import java.io.IOException;
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        try (java.io.FileReader fs = new java.io.FileReader(file.getAbsolutePath())) {
-            int ch;
-            StringBuilder sb = new StringBuilder();
-            while((ch=fs.read()) != -1) {
-                sb.append((char)ch);
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
+            String name = null;
+            String email = null;
+            int age = 0;
+            long phone = 0;
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(": ", 2);
+                if(parts.length < 2) continue;
+
+                String key = parts[0].trim();
+                String value = parts[1].trim();
+
+                switch (key) {
+                    case "Name": {
+                        name = value;
+                    };
+                    break;
+                    case "Email": {
+                        email = value;
+                        break;
+                    }
+                    case "Age": {
+                        age = Integer.parseInt(value);
+                        break;
+                    }
+                    case "Phone": {
+                        phone = Long.parseLong(value);
+                        break;
+                    }
+                }
             }
-            String[] keyvalues = sb.toString().split("\n");
-            String[] values = new String[keyvalues.length];
-            for (int j = 0; j < keyvalues.length; j++) {
-                String value = keyvalues[j].trim().split(": ")[1];
-                values[j] = value.trim();
-            }
-            return new Profile(values[0], Integer.parseInt(values[1]), values[2], (long)Integer.parseInt(values[3]));
+            return new Profile(name, age, email, phone);
         }
         catch (FileNotFoundException e) {
             e.printStackTrace();
